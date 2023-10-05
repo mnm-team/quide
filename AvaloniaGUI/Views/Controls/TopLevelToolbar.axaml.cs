9c53ab187fa80fce30d80f2c1fc69ca2ff752be2
Marc Fichtner <marc.a.fichtner@gmail.com>
2023-10-05 12:16:01 +0200

    outsourced top toolbar

---
+++ AvaloniaGUI/Views/Controls/TopLevelToolbar.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaGUI.Views.Controls;
+
+public partial class TopLevelToolbar : UserControl
+{
+    public TopLevelToolbar()
+    {
+        InitializeComponent();
+    }
+}
